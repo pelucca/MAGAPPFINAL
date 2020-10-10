c399fcc5402fd07d568bbe9d3a204b996b570062
RenanMarozzi <72401989+RenanMarozzi@users.noreply.github.com>
2020-10-10 10:18:59 -0300

    atualizando projeto

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  text = 'Meu texto sobre algum assunto';
+  
   constructor() { }
 
   ngOnInit() {
